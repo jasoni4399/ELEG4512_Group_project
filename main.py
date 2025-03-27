12fdedb7b709972eb493370e3ee1c50ae07acc78
jasoni <73955487+jasoni4399@users.noreply.github.com>
2025-03-27 21:12:46 +0800

    Update main.py

--- main.py
+++ main.py
@@ -9,9 +9,9 @@ from processors import process_image,save_image
 path = "inputs/blur_noisey_photo.jpg"
 image = cv2.imread(path)
 
-@process_image(image_path="histogram_equalization")#file name to save the image
+@process_image(image_path="adaptive_histogram_equalization")#file name to save the image
 def main(image):
-    processed_image = rgb_histogram_equalization(image)
+    processed_image = rgb_adaptive_histogram_equalization(image, clip_limit=2.0, grid_size=(8, 8))
     return processed_image
 
 if __name__ == "__main__":
