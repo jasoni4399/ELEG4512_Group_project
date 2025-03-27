f7629ba2b93189eafae4287e88e6719ead59b7b2
jasoni <73955487+jasoni4399@users.noreply.github.com>
2025-03-27 21:12:25 +0800

    Update __init__.py

--- processors/__init__.py
+++ processors/__init__.py
@@ -6,6 +6,8 @@ __all__ = ["remove_reflection",
             "rgb_laplacian_filter",
             "histogram_equalization",
             "rgb_histogram_equalization",
+            "adaptive_histogram_equalization",
+            "rgb_adaptive_histogram_equalization"
             ]
 
 from .remove_reflection import remove_reflection_test as remove_reflection
@@ -13,7 +15,7 @@ from .gamma_correction import gamma_correction
 from .laplacian_filtering import laplacian_filter as laplacian_filter
 from .laplacian_filtering import rgb_laplacian_filter as rgb_laplacian_filter
 from .histogram_equalization import histogram_equalization as histogram_equalization
-from .histogram_equalization import rgb_histogram_equalization as rgb_histogram_equalization
+from .histogram_equalization import *
 
 import cv2
 import os
