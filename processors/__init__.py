a11d334928afb3415db249a18a5f557e4c53075a
jasoni <73955487+jasoni4399@users.noreply.github.com>
2025-03-27 19:42:02 +0800

    update

--- processors/__init__.py
+++ processors/__init__.py
@@ -1,8 +1,21 @@
 __all__ = ["remove_reflection", "save_image", "process_image","gamma_correction"]
 
-from .test import remove_reflection_test as remove_reflection
-from .test import save_image
-from .test import process_image
-from .test import remove_reflection_test as remove_reflection
+from .remove_reflection import remove_reflection_test as remove_reflection
 from .gamma_correction import gamma_correction
 
+import cv2
+import os
+
+def save_image(image, path):
+    output_path = os.path.join("outputs", path+".jpg")
+    cv2.imwrite(output_path, image)
+    print(f"Processed image saved to: {output_path}")
+
+def process_image(image_path):
+    def wrapper(func):
+        def run(*args, **kwargs):
+            processed_image = func(*args, **kwargs)
+            save_image(processed_image, image_path)
+            return processed_image
+        return run
+    return wrapper
