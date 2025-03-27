8410d9eb7b5078a82be8a86f6d00faa54e370060
TYYJ1576 <130568371+TYYJ1576@users.noreply.github.com>
2025-03-27 19:51:20 +0800

    new

--- main.py
+++ main.py
@@ -3,17 +3,17 @@ import os
 from processors import remove_reflection
 from processors import gamma_correction
 from processors import save_image, process_image
+from processors import laplacian_filter
 
 
 # make a decorator to time the function
 path = "inputs/blur_noisey_photo.jpg"
 image = cv2.imread(path)
 
-@process_image(image_path="gamma_correction_10")
+@process_image(image_path="laplacian_filter.jpg")
 def main(image):
-    processed_image = processed_image = gamma_correction(image, 10)
+    processed_image = processed_image = laplacian_filter(image, 3)
     return processed_image
-
 
 if __name__ == "__main__":
     main(image)
