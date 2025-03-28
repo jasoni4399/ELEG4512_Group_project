3aa9ef241e2fa53e11d2caf2418d58fef9c658fb
jasoni <73955487+jasoni4399@users.noreply.github.com>
2025-03-28 20:55:12 +0800

    update

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
 import cv2
 import os
 import numpy as np
-from processors import remove_reflection
 from processors import *
-from processors import process_image,save_image
 
 # make a decorator to time the function
 path = "inputs/blur_noisey_photo.jpg"
 image = cv2.imread(path)
 
-@process_image(image_path="adaptive_histogram_equalization")#file name to save the image
+@process_image(image_path="test")#file name to save the image
 def main(image):
-    processed_image = rgb_adaptive_histogram_equalization(image, clip_limit=2.0, grid_size=(8, 8))
-    return processed_image
+    processed_image = image.copy()
+    for i in range(5):
+        temp = gamma_correction(processed_image, gamma=20)
+        processed_image = cv2.addWeighted(processed_image, 0.5, temp, 0.5, 0)
+    processed_image = rgb_histogram_equalization(processed_image)
+    #image - processed_image
+    
+
+    return image - processed_image
 
 if __name__ == "__main__":
     main(image)
--- processors/gamma_correction.py
+++ processors/gamma_correction.py
@@ -3,7 +3,7 @@ import numpy as np
 
 def gamma_correction(image, gamma=1.0, c=1.0):
 
-    normalized = image.astype(np.float32) / 255.0
+    normalized = image.copy().astype(np.float32) / 255.0
     result = c * np.power(normalized, gamma)
     result = np.clip(result * 255, 0, 255).astype(np.uint8)
 
