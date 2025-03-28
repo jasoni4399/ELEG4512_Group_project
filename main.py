372ccb341577a836fa92e22d888b530f41c910f4
TYYJ1576 <130568371+TYYJ1576@users.noreply.github.com>
2025-03-28 23:30:03 +0800

    new

--- main.py
+++ main.py
@@ -1,25 +1,30 @@
 import cv2
 import os
 import numpy as np
-from processors import frequency_domain_separation, generate_reflection_mask, multi_scale_inpainting, contrast_stretching, histogram_normalization, laplacian_filter, gamma_correction, contrast_stretching, average_filter, gaussian_blurring
-from processors import process_image, median_filter_2d, dilation, erosion
-
+from processors import *
 # make a decorator to time the function
-path = "inputs/old_image.jpg"
+path = "inputs/blur_noisey_photo.jpg"
 image = cv2.imread(path)
 
 @process_image(image_path="frequency_domain_separation")#file name to save the image
 def main(image):
-    image_hist = histogram_normalization(image, min_out=20, max_out=240)
-    image_gamma = gamma_correction(image_hist, gamma=2)
 
-    edge_laplcian = laplacian_filter(image_gamma, kernel_size=5)
-    image = cv2.cvtColor(edge_laplcian, cv2.COLOR_BGR2GRAY)
-    print(image.shape)
-    kernel = np.ones((3, 3), dtype=np.uint8)
-    outputs = dilation(image, kernel)
+    outputs = generate_reflection_mask(image, kernal_size=5, threshold=145)
+    laplacian = laplacian_filter(outputs, kernel_size=5)
+    gamma = gamma_correction(image, 10)
+    outputs2 = generate_reflection_mask(gamma, kernal_size=5, threshold=120)
+    outputs2 = histogram_normalization(outputs2, 0, 255)
+    laplacian2 = laplacian_filter(outputs2, kernel_size=5)
+    laplacian3 = laplacian + laplacian2
+    dilation_kernel = np.ones((5,5), np.uint8)
+    thick_edge = cv2.dilate(laplacian3, dilation_kernel, iterations=2)
+    #thick_edge = cv2.erode(laplacian3, dilation_kernel, iterations=1)
+    
+    
 
-    return outputs
+    laplacian3_3channel = cv2.cvtColor(laplacian3, cv2.COLOR_GRAY2BGR)
+    final = cv2.add(image, laplacian3_3channel)
+    return outputs2
 
 if __name__ == "__main__":
     main(image)
