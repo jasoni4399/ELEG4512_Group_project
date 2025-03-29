d10169df7c43f0e5a697851afdf7d96381f30dae
TYYJ1576 <130568371+TYYJ1576@users.noreply.github.com>
2025-03-29 15:12:32 +0800

    new

--- edge_detection_process_test.py
+++ edge_detection_process_test.py
@@ -8,10 +8,10 @@ path = "inputs/blur_noisey_photo.jpg"
 image = cv2.imread(path)
 show_result = False
 
-@process_image(image_path="edge_detection_outputs/_log/Log_3x3")#file name to save the image
+@process_image(image_path="edge_detection_outputs/sobel/sobel_5_1_1_False_False")#file name to save the image
 def main(image):
 
-    #output = sobel(image, ksize=3, dx=1, dy=1, normalize=False, uint8=False, show_result=True)
+    output = sobel(image, ksize=5, dx=1, dy=1, normalize=False, uint8=False, show_result=True)
     #output = laplacian(image, ksize=3, normalize=False, uint8=True, show_result=True)
     #output = LoG(image, ksize=3, sigma=1.0, normalize=False, show_result=True)
     #output = canny(image, threshold1=100, threshold2=300, apertureSize=3, normalize=False, show_result=True)
